e7f907e842ea487cad93e3c99136e01f72ebf3f8
Vinicius Santos <vilucenasantos@gmail.com>
2019-07-17 21:29:33 -0300

    mongo connection created

---
+++ Winap/Services/PersonService.cs
@@ -0,0 +1,48 @@
+using Winap.Models.Interfaces;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using Winap.Models;
+
+namespace Winap.Services
+{
+    public class PersonService
+    {
+        private readonly IMongoCollection<Person> _persons;
+
+        public PersonService(IWinnerDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _persons = database.GetCollection<Person>(settings.WinnerCollectionName);
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            return _persons.Find(person => true).ToList();
+        }
+
+        public Person GetById(int id)
+        {
+            return _persons.Find<Person>(person => person.Id == id).FirstOrDefault();
+        }
+
+        public Person Create(Person person)
+        {
+            _persons.InsertOne(person);
+            return person;
+        }
+
+        public void Update(int id, Person newPerson)
+        {
+            _persons.ReplaceOne(person => person.Id == id, newPerson);
+        }
+
+        public void Remove(int id)
+        {
+            _persons.DeleteOne(person => person.Id == id);
+        }
+
+    }
+}
